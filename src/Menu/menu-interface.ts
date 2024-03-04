385ca06ed62fe3eae6f9ee01db146bb23be57418
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2024-03-04 16:28:44 +0100

    Reorganizing Menu file, adding interface file for menu, removing styles.d.ts (as it is working better witout it) and creating App in index.tsx (#1)

---
+++ src/Menu/menu-interface.ts
@@ -0,0 +1,37 @@
+import React, { ButtonHTMLAttributes, Key, RefObject } from "react";
+import { AriaMenuProps, MenuTriggerProps } from "@react-types/menu";
+import { Node } from "@react-types/shared";
+import { TreeState } from "@react-stately/tree";
+import { MenuTriggerState } from "@react-stately/menu";
+
+// AriaMenuProps<T> related to the CollectionChildren
+// MenuTriggerProps necessary to be used
+// on the useButton() on SubMenu Component
+export type SapphireSubMenuProps<T extends object> = AriaMenuProps<T> &
+  MenuTriggerProps & {
+    renderTrigger: (
+      props: ButtonHTMLAttributes<Element> & { ref?: RefObject<any> },
+      isOpen: boolean
+    ) => React.ReactNode;
+    refTriggerItem: RefObject<HTMLLIElement>;
+    statePopOver: MenuTriggerState;
+    shouldFlip?: boolean;
+  };
+
+export type SapphireMenuProps<T extends object> = AriaMenuProps<T> &
+  MenuTriggerProps & {
+    renderTrigger: (
+      props: ButtonHTMLAttributes<Element> & { ref?: RefObject<any> },
+      isOpen: boolean
+    ) => React.ReactNode;
+    shouldFlip?: boolean;
+  };
+
+export interface MenuItemProps<T> {
+  item: Node<T>;
+  state: TreeState<T>;
+  onAction?: (key: Key) => void;
+  onClose?: () => void;
+  disabledKeys?: Iterable<Key>;
+  shouldFlip?: boolean;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,32 @@
-import { SetStateAction, StrictMode } from "react";
+import React, { SetStateAction, StrictMode } from "react"; // Ensure React is imported
 import { createRoot } from "react-dom/client";
 
 import { Menu, Item } from "./Menu";
 
+// Define the App component
+function App() {
+  return (
+    <StrictMode>
+      <Menu
+        renderTrigger={(props) => <button {...props}>Actions</button>}
+        onAction={alert}
+        shouldFlip={true}
+      >
+        <Item key="copy">Copy application</Item>
+        <Item key="rename">Rename application</Item>
+        <Item key="move" title="Move to">
+          <Item key="move-to-shared">Shared</Item>
+          <Item key="move-to-desktop">Desktop</Item>
+          <Item key="move-to-favorite">Favorite</Item>
+        </Item>
+        <Item key="delete">Delete application</Item>
+      </Menu>
+    </StrictMode>
+  );
+}
+
+// Render the App component to the DOM
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement || document.createElement("div"));
 
-root.render(
-  <StrictMode>
-    <Menu
-      renderTrigger={(props) => <button {...props}>Actions</button>}
-      onAction={alert}
-      shouldFlip={true}
-    >
-      <Item key="copy">Copy application</Item>
-      <Item key="rename">Rename application</Item>
-      <Item key="move" title="Move to">
-        <Item key="move-to-shared">Shared</Item>
-        <Item key="move-to-desktop">Desktop</Item>
-        <Item key="move-to-favorite">Favorite</Item>
-      </Item>
-      <Item key="delete">Delete application</Item>
-    </Menu>
-  </StrictMode>
-);
+root.render(<App />);
